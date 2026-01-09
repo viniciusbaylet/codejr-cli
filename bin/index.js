21ff12dcbe5a8dea8405bc7bee86e20cee42f339
ViniciusBaylet <viniciusbaylet6@gmail.com>
2026-01-09 12:22:31 -0300

    adiciona o nome da code antes do prompt iniciar

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,15 @@ import { setupGit } from "../src/git/setupGit.js";
 import { setupCommitStandard } from "../src/commit/setupCommitStandard.js";
 
 async function main() {
+
+  console.log(`
+   ______          __             __            _
+  / ____/___  ____/ /__          / /_  ______  (_)___  _____
+ / /   / __ \\/ __  / _ \\    __  / / / / / __ \\/ / __ \\/ ___/
+/ /___/ /_/ / /_/ /  __/   / /_/ / /_/ / / / / / /_/ / /
+\\____/\\____/\\__,_/\\___/    \\____/\\__,_/_/ /_/_/\\____/_/
+`);
+
   const { projectName, tech } = await promptProject();
 
   const projectNameSafe = projectName
