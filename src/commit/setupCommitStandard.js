f91aa4cfcf8583c917c739f9b25bcd86dcf1551c
ViniciusBaylet <viniciusbaylet6@gmail.com>
2026-01-09 10:26:03 -0300

    corrige setup do commit

--- src/commit/setupCommitStandard.js
+++ src/commit/setupCommitStandard.js
@@ -3,40 +3,62 @@ import path from "path";
 import { run } from "../utils/exec.js";
 
 export function setupCommitStandard() {
-  console.log("📦 Configurando padrão de commits da CodeJR...\n");
+    console.log("📦 Configurando padrão de commits da CodeJR...\n");
 
-  // Dependências
-  run(
-    "npm install --save-dev husky @commitlint/cli @commitlint/config-conventional commitizen cz-conventional-changelog"
-  );
+    // Instalar dependências
+    run(
+        "npm install --save-dev husky @commitlint/cli @commitlint/config-conventional commitizen cz-conventional-changelog"
+    );
 
-  // commitlint.config.cjs
-  fs.writeFileSync(
-    "commitlint.config.cjs",
-    `module.exports = {
+    // commitlint.config.cjs
+    fs.writeFileSync(
+        "commitlint.config.cjs",
+        `module.exports = {
   extends: ['@commitlint/config-conventional'],
 };
 `
-  );
+    );
 
-  // package.json
-  const pkgPath = path.resolve("package.json");
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
+    // Atualizar package.json
+    const pkgPath = path.resolve("package.json");
+    const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
 
-  pkg.scripts ||= {};
-  pkg.scripts.commit = "git cz";
+    pkg.scripts ||= {};
+    pkg.scripts.prepare ||= "husky";
+    pkg.scripts.commit = "git cz";
 
-  pkg.config ||= {};
-  pkg.config.commitizen = {
-    path: "cz-conventional-changelog",
-  };
+    pkg.config ||= {};
+    pkg.config.commitizen = {
+        path: "cz-conventional-changelog",
+    };
 
-  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
 
-  // Husky
-  run("npx husky init");
+    // Inicializar Husky
+    run("npx husky install");
 
-  run(
-    "npx husky add .husky/commit-msg \"npx --no-install commitlint --edit $1\""
-  );
+    // Criar hook commit-msg MANUALMENTE
+    const huskyDir = path.resolve(".husky");
+    if (!fs.existsSync(huskyDir)) {
+        fs.mkdirSync(huskyDir);
+    }
+
+    const hookPath = path.join(huskyDir, "commit-msg");
+
+    fs.writeFileSync(
+        hookPath,
+        `
+            #!/bin/sh
+            . "$(dirname "$0")/_/husky.sh"
+
+            npx --no-install commitlint --edit "$1"
+        `
+    );
+
+    // Garantir permissão (Linux/macOS – Windows ignora)
+    try {
+        fs.chmodSync(hookPath, 0o755);
+    } catch { }
+
 }
+
