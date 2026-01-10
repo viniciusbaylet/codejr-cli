991e033cd41129c4b8137f5039f3ef9e4c3b8324
ViniciusBaylet <viniciusbaylet6@gmail.com>
2026-01-10 16:53:17 -0300

    altera layout novamente denovo

--- src/ui/banner.js
+++ src/ui/banner.js
@@ -4,27 +4,21 @@ import figlet from "figlet";
 export function showBanner() {
   console.clear();
 
-  const code =  chalk.hex("#8b5cf6")(figlet.textSync("Code", {
+  const title =  chalk.hex("#8b5cf6")(figlet.textSync("Code Junior", {
     horizontalLayout: "default",
   }));
-
-  const junior =  chalk.yellow(figlet.textSync("Junior", {
-    horizontalLayout: "default",
-  }));
-
-  const title = code + " " + junior;
 
   console.log(title);
 
   console.log(
-    chalk.hex("#8b5cf6")("=".repeat(80))
+    chalk.yellow("=".repeat(80))
   );
 
   console.log(
-    chalk.bold.yellow("   🚀  GERADOR DE PROJETOS | CODE JÚNIOR")
+    chalk.bold.cyan("   🚀  GERADOR DE PROJETOS | CODE JÚNIOR")
   );
 
   console.log(
-    chalk.hex("#8b5cf6")("=".repeat(80))
+    chalk.yellow("=".repeat(80))
   );
 }
